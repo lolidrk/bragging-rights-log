a3491389ef7f734c55152109729a5ea9db0330d5
kalyani <kalyanikulkarni2002@gmail.com>
2025-05-03 15:53:45 +0530

    Trivial fix

--- 169.MajorityElement.cpp
+++ 169.MajorityElement.cpp
@@ -11,17 +11,4 @@ public:
         }
         return 0;
     }
-};class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        unordered_map<int, int> count;
-        for (int num: nums){
-            count[num]++;
-            if (count[num] > (nums.size()/2))
-                return num;
-        }
-        return 0;
-    }
 };
