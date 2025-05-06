60eb7cb8e973e75b843fd4fe62a6071b44c69334
kalyani <kalyanikulkarni2002@gmail.com>
2025-05-06 21:59:19 +0530

    [Easy] 1920. Build array from permutation

---
+++ 1920.BuildArrayFromPermutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i){
+            nums[i] = nums[i] + (nums[nums[i]]% nums.size())*nums.size();
+        }
+        for (int i = 0; i < nums.size(); ++i){
+            nums[i] = nums[i] / nums.size();
+        }
+
+        return nums;
+    }
+};
