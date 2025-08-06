88e4ef79d0515ed68a84754f3ea2559d1ca1b86d
kalyani <kalyanikulkarni2002@gmail.com>
2025-08-07 01:29:15 +0530

    [Easy] 169:Majority Element

---
+++ Kali-code/169.MajorityElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if (nums.size() == 1)
+            return nums[0];
+        unordered_map<int, int> count;
+        for (int num: nums){
+            count[num]++;
+            if (count[num] > (nums.size()/2))
+                return num;
+        }
+        return 0;
+    }
+};
